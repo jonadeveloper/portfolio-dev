d7955162dabd39962a7968b825c98a7392599390
jonatan villalva <jvillalva.sistemas@gmail.com>
2025-05-26 10:01:53 -0300

    agregando configuraciones de vercel para el deploy

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/static";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    imageService: true,
+  }),
 });
